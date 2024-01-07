971165f0d1aa1c6602830b8678fccd070aa39e78
Dmitry Zhutkov <dmitry.zhutkov@gmail.com>
2024-01-07 12:32:31 +0500

    Added new styled controls, changed icon and other minor changes

---
+++ src/PleasantUI/Converters/TreeViewItemMarginConverters.cs
@@ -0,0 +1,12 @@
+﻿using Avalonia.Controls.Converters;
+
+namespace PleasantUI.Converters;
+
+public class TreeViewItemMarginConverters
+{
+    public static readonly MarginMultiplierConverter Left = new()
+    {
+        Indent = 16,
+        Left = true
+    };
+}
