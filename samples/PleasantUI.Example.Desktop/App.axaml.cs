916a1e3026cf30bbc9d874b508b384a0b8781eed
JitDaKid <yalehunter333@gmail.com>
2025-02-26 14:34:50 -0800

    Small changes for macOS UI

--- samples/PleasantUI.Example.Desktop/App.axaml.cs
+++ samples/PleasantUI.Example.Desktop/App.axaml.cs
@@ -1,6 +1,7 @@
 using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Avalonia.Platform;
 using PleasantUI.Controls;
 
 namespace PleasantUI.Example.Desktop;
@@ -13,26 +14,31 @@ public partial class App : PleasantUiExampleApp
     {
         if (ApplicationLifetime is not IClassicDesktopStyleApplicationLifetime desktop)
             return;
-        
+
         if (Design.IsDesignMode)
         {
             desktop.MainWindow = new PleasantWindow();
-            
+
             base.OnFrameworkInitializationCompleted();
             return;
         }
-        
+
         PleasantTheme = Styles[0] as PleasantTheme ?? throw new NullReferenceException("PleasantTheme is null");
-        
+
         Main = new MainWindow
         {
             DataContext = ViewModel
         };
 
         TopLevel = TopLevel.GetTopLevel(Main as PleasantWindow) ?? throw new NullReferenceException("TopLevel is null");
-            
+
         desktop.MainWindow = Main as PleasantWindow;
-        
+
+        if (OperatingSystem.IsMacOS() && desktop.MainWindow != null)
+        {
+            desktop.MainWindow.ExtendClientAreaChromeHints = ExtendClientAreaChromeHints.OSXThickTitleBar;
+        }
+
         base.OnFrameworkInitializationCompleted();
     }
 }
